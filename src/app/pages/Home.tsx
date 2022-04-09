c1104baee612af2043461c03e788149cd9b2e175
johnkorheledu <johnkorhel1@gmail.com>
2022-04-09 11:41:28 -0600

    fix issues

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,4 +1,5 @@
-import {Typography} from "@mui/material";
+import {Grid, Typography} from "@mui/material";
+import {ContainerGrid} from "../components/layout/ContainerGrid";
 
 export function Home() {
     return (
